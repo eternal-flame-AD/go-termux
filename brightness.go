fda8ef8d14534d9131393ac09d2c5ce67fc10b17
eternal-flame-AD <ef@eternalflame.info>
2019-02-12 12:56:38 +0800

    add auto brightness

--- brightness.go
+++ brightness.go
@@ -9,6 +9,19 @@ func Brightness(level uint8) error {
 	buf := bytes.NewBuffer([]byte{})
 	if err := exec(nil, buf, "Brightness", map[string]interface{}{
 		"brightness": int(level),
+		"auto":       false,
+	}, ""); err != nil {
+		return err
+	}
+	res := buf.Bytes()
+	return checkErr(res)
+}
+
+// BrightnessAuto sets the current brightness to auto
+func BrightnessAuto() error {
+	buf := bytes.NewBuffer([]byte{})
+	if err := exec(nil, buf, "Brightness", map[string]interface{}{
+		"auto": true,
 	}, ""); err != nil {
 		return err
 	}
